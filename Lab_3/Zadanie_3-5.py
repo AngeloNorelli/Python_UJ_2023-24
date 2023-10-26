eff05e610b20882b1bed07866b9b42cad7f49a76
AngeloNorelli <angnorelli@gmail.com>
2023-10-26 14:44:30 +0200

    Zadanie 3.5

---
+++ Lab_3/Zadanie_3-5.py
@@ -0,0 +1,15 @@
+def Miarka(length):
+    space_between = len(str(length)) * 2
+    x = "|"
+    y = "0"
+
+    for i in range(length):
+        x += "".zfill(space_between).replace('0', '.') + '|'
+        y += "".zfill(space_between + 1 - len(str(i+1))).replace('0', ' ') + str(i+1)
+
+    print(x)
+    print(y)
+
+
+user_input = input("Podaj rozmiar miarki (liczba calkowita): ")
+Miarka(int(user_input))
