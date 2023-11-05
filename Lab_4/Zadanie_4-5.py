3a9639662577e5b13784dc7ab9bc6553c77a7a58
AngeloNorelli <angnorelli@gmail.com>
2023-11-05 12:37:16 +0100

    Zadanie 4.5 po zrozumieniu zadania do konca

--- Lab_4/Zadanie_4-5.py
+++ Lab_4/Zadanie_4-5.py
@@ -1,5 +1,29 @@
-def reverse_list(L):
-    return L[::-1]
+def it_odwracanie(L, left, right):
+    if left < 0:
+        return 'left too small'
+    if right >= len(L):
+        return 'right too big'
+
+    while left < right:
+        L[left], L[right] = L[right], L[left]
+        left += 1
+        right -= 1
+    
+    return L
+
+def rec_odwracanie(L, left, right):
+    if left < 0:
+        return 'left too small'
+    if right >= len(L):
+        return 'right too big'
+
+    if left < right:
+        L[left], L[right] = L[right], L[left]
+        rec_odwracanie(L, left+1, right-1)
+    
+    return L
 
 L=[1, 2, 3, 5, 7]
-print(reverse_list(L))
+print(f"Lista przed odwroceniem: {L}")
+print(f"Lista po wykonaniu it_odwracanie(L, 1, 4): {it_odwracanie(L, 1, 4)}")
+print(f"Lista po rec_obracanie(L, 0, 3): {rec_odwracanie(L, 0, 3)}")
