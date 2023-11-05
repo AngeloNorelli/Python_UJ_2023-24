053c63a7b7b87aac11fe18c07cd35088649fa9a6
AngeloNorelli <angnorelli@gmail.com>
2023-11-05 11:09:35 +0100

    Zadanie 4.2

---
+++ Lab_4/Zadanie_4-2.py
@@ -0,0 +1,36 @@
+def make_ruler(n):
+    space_between = len(str(n)) * 2
+    if space_between < 4:
+        space_between = 4
+    
+    x = "|"
+    y = "0"
+
+    for i in range(n):
+        x += "".zfill(space_between).replace('0', '.') + '|'
+        y += "".zfill(space_between + 1 - len(str(i+1))).replace('0', ' ') + str(i+1)
+
+    return x + '\n' + y
+
+def make_grid(rows, cols):
+    walls = "   |"
+    celling = "---+"
+
+    result1 = "+"
+    result2 = "|"
+    result = ''
+
+    for i in range(rows):
+        result1 += celling
+        result2 += walls
+
+    for i in range(cols):
+        result += result1 + '\n'
+        result += result2 + '\n'
+    result += result1
+    return result
+
+result = make_ruler(10)
+print(result)
+result = make_grid(2, 4)
+print(result)
