6422b999e254494db98ac5891349c11afc9ca61d
AngeloNorelli <angnorelli@gmail.com>
2023-12-09 00:44:06 +0100

    Ustawienia

--- Lab_9/settings.py
+++ Lab_9/settings.py
@@ -4,6 +4,10 @@ vec = pg.math.Vector2
 
 FPS = 60
 FIELD_COLOR = (48, 39, 32)
+BG_COLOR = (24, 89, 117)
+
+SPRITE_DIR_PATH = 'Python_UJ_2023-24/Lab_9/sprites'
+FONT_PATH = 'Python_UJ_2023-24/Lab_9/font/Silkscreen-Regular.ttf'
 
 ANIM_TIME_INTERVAL = 150    # milliseconds
 FAST_ANIM_TIME_INTERVAL = 15
@@ -12,7 +16,11 @@ TILE_SIZE = 50
 FIELD_SIZE = FIELD_W, FIELD_H = 10, 20
 FIELD_RES = FIELD_W * TILE_SIZE, FIELD_H * TILE_SIZE
 
+FIELD_SCALE_W, FIELD_SCALE_H = 1.7, 1.0
+WIN_RES = WIN_W, WIN_H = FIELD_RES[0] * FIELD_SCALE_W, FIELD_RES[1] * FIELD_SCALE_H
+
 INIT_POS_OFFSET = vec(FIELD_W // 2 - 1, 0)
+NEXT_POS_OFFSET = vec(FIELD_W * 1.3, FIELD_H * 0.45)
 MOVE_DIRECTIONS = {'left': vec(-1,0), 'right': vec(1,0), 'down': vec(0,1),}
 
 TETROMINOES = {
